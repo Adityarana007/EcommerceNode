a602c11592ec55361c8b4660c3fb788c13cec618
Aditya Rana <ranaaditya104@gmail.com>
2025-05-10 23:36:46 +0530

    Bug fix

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/api/protected', auth, (req, res) => {
 // Routes
 // app.use('/api/auth', require('./routes/authRoutes'));
 app.get('/api/user/:id', async (req, res) => {
-    console.log(req.params.id)
+    // console.log(req.params.id)
     const existingUser = await User.findOne({ userId: req.params.id });
     if(!existingUser) return res.status(404).json({error: 'user not found'})
     return res.json(existingUser);
@@ -49,12 +49,19 @@ app.get('/api/user/:id', async (req, res) => {
 
   app.post('/api/register', async (req, res) => {
     const body = req.body;
-    console.log('BODY', body);
-
     try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(body.password, salt);
+        const hashedConfirmPassword = await bcrypt.hash(body.confirmPassword, salt);
+
+        if(body.password !== body.confirmPassword){
+            res.status(400).json({ error: "Password and confirm password doesnot match.", statusCode: 400 });
+        }
+
         // Check if the email already exists
         const existingUser = await User.findOne({ email: body.email });
-        console.log('existingUser', existingUser);
+        console.log('existingUser', existingUser)
+        // console.log('existingUser', existingUser);
         if (existingUser) {
             return res.status(400).json({ error: 'User already exists.', statusCode: 400 });
         }
@@ -63,11 +70,6 @@ app.get('/api/user/:id', async (req, res) => {
         const lastUser = await User.findOne().sort({ userId: -1 }); // Sort by userId in descending order
         const newUserId = lastUser ? lastUser.userId + 1 : 1; // Increment or start with 1
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(body.password, salt);
-        const hashedConfirmPassword = await bcrypt.hash(body.confirmPassword, salt);
-
-
         // Create a new user with the incremented userId
         const result = await User.create({
             email: body.email,
@@ -75,7 +77,6 @@ app.get('/api/user/:id', async (req, res) => {
             confirmPassword: hashedConfirmPassword,
             userId: newUserId
         });
-
         return res.status(201).json({
             message: 'User created successfully',
             statusCode: 201,
